b0006acc7ea234d499666a48dc02b2b9192c7afa
hq525 <zhaohanqing96@gmail.com>
2023-09-07 01:12:17 +0800

    feat: add axios

--- session3/src/hoc/Layout.js
+++ session3/src/hoc/Layout.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Outlet, Link } from "react-router-dom";
 
 const Layout = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                </ul>
+            </nav>
+
+            <Outlet />
+        </>
     );
 };
 
